28630693e9756fbe78300174f6df200dcac1e483
Prachi <prachikale136@gmail.com>
2023-07-14 11:39:58 +0530

    [Prachi] Add: Add new coloumn to the table

--- SqlPracticeProblem/SqlPracticeProblem/SQLQuery2.sql
+++ SqlPracticeProblem/SqlPracticeProblem/SQLQuery2.sql
@@ -10,3 +10,32 @@ insert into customer( Name,PhoneNo, Address) values('Prachi','896541223','mumbai
 ('Amol','9654123211', 'Delhi'),
 ('PAwan', '88521366145', 'NAgpur'),
 ('keshav', '987456321', 'Indore')
+
+alter table customer add
+ Country varchar(10),
+ Salary money,
+ pincode varchar(6)
+
+ update customer set Country = 'India',
+ salary = 50000.0,
+ pincode = '456321'
+  where Id = 1;
+
+  update customer set Country = 'US',
+ salary = 55000.0,
+ pincode = '996322'
+  where Id = 2;
+
+  update customer set Country = 'india',
+ salary = 45000.0,
+ pincode = '12345'
+  where Id = 3;
+
+  update customer set Country = 'india',
+ salary = 35000.0,
+ pincode = '789665'
+  where Id = 4;
+
+  select * from customer
+
+
