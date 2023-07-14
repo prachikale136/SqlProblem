d5f71c1cc8c96c4a32c23cf435aeea463f063663
Prachi <prachikale136@gmail.com>
2023-07-14 11:20:03 +0530

    [Prachi] Add: Create table of customer

--- SqlPracticeProblem/SqlPracticeProblem/SQLQuery2.sql
+++ SqlPracticeProblem/SqlPracticeProblem/SQLQuery2.sql
@@ -1,9 +1,7 @@
-﻿create Database customerDB
-use customerDB
-
-create table customer(
+﻿create table customer(
  Id int identity(1,1) primary key ,
  Name varchar(15),
  PhoneNo varchar(15),
  Address varchar(50)
 );
+select * from customer
